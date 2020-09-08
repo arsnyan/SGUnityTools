121a5dc92382c31f21fbea961d26c2805cc99998
beautyadore <45586929+beautyadore@users.noreply.github.com>
2020-09-08 19:43:49 +0300

    Add files via upload

--- Editor/ScreenshotTool.cs
+++ Editor/ScreenshotTool.cs
@@ -1,34 +1,51 @@
-﻿using System;
+﻿/**************************************************************************
+ * Copyright 2020 ArsMania
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**************************************************************************/
+
+using System;
 using UnityEditor;
 using UnityEngine;
 
-namespace Editor
+namespace SonicUnleashed.Editor
 {
-	public class ScreenshotTool : EditorWindow
-	{
-		private static string _fileName = "Screenshot_";
-		private static int _sizeScale = 1;
-	
-		private static DateTime _dt;
-	
-		[MenuItem("Window/Take a screenshot")]
-		public static void Init()
-		{
-			EditorWindow window = GetWindow(typeof(ScreenshotTool));
-			_dt = new DateTime();
-			window.Show();
-		}
+    public class ScreenshotTool : EditorWindow
+    {
+        private static string _fileName = "Screenshot_";
+        private static int _sizeScale = 1;
 
-		public void OnGUI()
-		{
-			_fileName = EditorGUILayout.TextField("File name", _fileName);
-			_sizeScale = EditorGUILayout.IntField("Size scale", _sizeScale);
-		
-			if (GUILayout.Button("Take a screenshot"))
-			{
-				ScreenCapture.CaptureScreenshot($"{_fileName + _dt.Month + _dt.Day + _dt.Hour + _dt.Second}.png", _sizeScale);
-				Debug.Log("Screenshot is saved");
-			}
-		}
-	}
+        private static DateTime _dt;
+
+        public void OnGUI()
+        {
+            _fileName = EditorGUILayout.TextField("File name", _fileName);
+            _sizeScale = EditorGUILayout.IntField("Size scale", _sizeScale);
+
+            if (GUILayout.Button("Take a screenshot"))
+            {
+                ScreenCapture.CaptureScreenshot($"{_fileName + _dt.Month + _dt.Day + _dt.Hour + _dt.Second}.png",
+                    _sizeScale);
+                Debug.Log("Screenshot is saved");
+            }
+        }
+
+        [MenuItem("Window/Take a screenshot")]
+        public static void Init()
+        {
+            var window = GetWindow(typeof(ScreenshotTool));
+            _dt = new DateTime();
+            window.Show();
+        }
+    }
 }
--- Editor/UVSwapper.cs
+++ Editor/UVSwapper.cs
@@ -1,8 +1,24 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-using UnityEditor;
+﻿/**************************************************************************
+ * Copyright 2020 ArsMania
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+**************************************************************************/
 
-namespace Editor
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace SonicUnleashed.Editor
 {
     public class UVSwapper : MonoBehaviour
     {
@@ -10,7 +26,7 @@ namespace Editor
         public static void SwapUVs(MenuCommand command)
         {
             var selected = Selection.gameObjects;
-            
+
             foreach (var obj in selected)
             {
                 var mesh = obj.GetComponent<MeshFilter>().sharedMesh;
